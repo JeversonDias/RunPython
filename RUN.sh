14365537c2bcb9d8b7106ecf4fd5fcbea895f9e6
JeversonDias <147354391+JeversonDias@users.noreply.github.com>
2024-09-13 19:56:17 +0000

    Update RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -8,6 +8,6 @@ mkdir -p "/userdata/system/.dev/scripts/python3"
 cd "/userdata/system/.dev/scripts/python3"
 wget https://raw.githubusercontent.com/JeversonDias/RunPython/main/python3.12.3-cp312-cp312-manylinux2014_x86_64.AppImage
 chmod +x "/userdata/system/.dev/scripts/python3/python3.12.3-cp312-cp312-manylinux2014_x86_64.AppImage"
-/userdata/system/.dev/scripts/python3/python3.12.3-cp312-cp312-manylinux2014_x86_64.AppImage -m venv myenv
+"/userdata/system/.dev/scripts/python3/python3.12.3-cp312-cp312-manylinux2014_x86_64.AppImage" -m venv myenv
 ln -s "/userdata/system/.dev/scripts/python3/myenv/bin/python3" /usr/bin/python3
 ln -s "/userdata/system/.dev/scripts/python3/myenv/bin/pip3" /usr/bin/pip3
